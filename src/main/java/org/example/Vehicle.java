a420bbad53a3c4a02dd0806d377b0e48883b7037
Hokage-Mani <armaniburton.nyc@gmail.com>
2026-05-07 21:19:26 -0400

    Inital commit

---
+++ src/main/java/org/example/Vehicle.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class Vehicle {
+    private int vin;
+    private int year;
+    private String make;
+    private String model;
+    private String vehicleType;
+    private String color;
+    private int odometer;
+    private double price;
+
+}
---
+++ src/main/java/org/example/VehicleType.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public enum VehicleType {
+    SEDAN,
+    TRUCK,
+    SUV,
+    VAN,
+    COUPE
+}
