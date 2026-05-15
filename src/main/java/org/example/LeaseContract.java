08af074d4930e9525de6c961f2604668c7f2410f
Hokage-Mani <armaniburton.nyc@gmail.com>
2026-05-15 18:43:04 -0400

    Filling my Total price for the leasing contract, here its only asking for ending value plus the lease fee so its calling the two methods to add them together

--- src/main/java/org/example/LeaseContract.java
+++ src/main/java/org/example/LeaseContract.java
@@ -1,6 +1,7 @@
 package org.example;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 
 public class LeaseContract extends Contract{
 
@@ -8,11 +9,19 @@ public class LeaseContract extends Contract{
     public LeaseContract(String date, String name, String email, Vehicle vehicleSold) {
         super(date, name, email, vehicleSold);
     }
+    private BigDecimal getEndingValue(){
+        double originalPrice = getVehicleSold().getPrice();
+        return BigDecimal.valueOf(originalPrice).multiply(new BigDecimal("0.50"));
+    }
+    private BigDecimal getLeaseFee(){
+        double originalPrice = getVehicleSold().getPrice();
+        return BigDecimal.valueOf(originalPrice).multiply(new BigDecimal("0.07"));
+    }
 
     @Override
     public BigDecimal getTotalPrice() {
-        //logic here
-        return null;
+        BigDecimal total = getEndingValue().add(getLeaseFee());
+        return total.setScale(2, RoundingMode.HALF_UP);
     }
 
     @Override
